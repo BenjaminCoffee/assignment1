1f9ae9b187433275616c4ef1e14bebd8902f5c13
benjaminp <benjamin7734@gmail.com>
2020-10-02 19:30:59 -0500

    Added getters and setters in CheckingAccount.java.

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -5,4 +5,19 @@ public class CheckingAccount {
 	private double balance;
 	private double interestRate;
 	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	
+	
 }
