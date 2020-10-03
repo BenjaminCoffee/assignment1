d81f287bdc1e5e7949f56d22b2f81ae9ebc78f90
benjaminp <benjamin7734@gmail.com>
2020-10-02 22:59:57 -0500

    Added a main method in MeritAmericaBankApp.java. Added withdraw method in CheckingAccount.java.

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -5,6 +5,19 @@ public class CheckingAccount {
 	private double balance;
 	private double interestRate;
 	
+	/*
+	 * Will display an error message if the withdraw amount exceeds the account
+	 * balance and return a boolean value of false.
+	 */
+	public boolean withdraw(double amount) {
+		if(amount > getBalance()) {
+			System.out.println("There is not enough money in the checking account "
+					+ "to make this widrawal");
+			return false;
+		}
+		else return true;
+	}
+	
 	public double getBalance() {
 		return balance;
 	}
--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,9 @@
 package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
+	public static void main(String[] args) {
+		AccountHolder accountHolder = new AccountHolder();
+		
+	}
 	
 }
