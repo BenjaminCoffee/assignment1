3c62fe6b05ee994993fe9c419c8e3b2be570a3d5
benjaminp <benjamin7734@gmail.com>
2020-10-02 19:27:00 -0500

    Added fields, getters and setters for SavingsAccount.java.

--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -2,4 +2,22 @@ package com.meritamerica.assignment1;
 
 public class SavingsAccount {
 	
+	private double balance;
+	private double interestRate;
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	
+	
 }
